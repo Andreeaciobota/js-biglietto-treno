60a7c3d66ef2a3d9c4226907732227924d2886e0
Andreea Ciobota <135983461+Andreeaciobota@users.noreply.github.com>
2023-09-07 18:05:58 +0200

    [Fix]:bug

--- js/script.js
+++ js/script.js
@@ -4,28 +4,43 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.*/
-const kilometersTravel = parseInt(prompt(`Quanti chilometri ti piacerebbe percorrere `));
-const ageTraveler = parseInt(prompt(`Indica la tua età!`));
+const kilometresTravel = parseInt(prompt('Quanti chilometri ti piacerebbe percorrere ?'));
+const ageTraveler = parseInt(prompt('Indica la tua età!'));
+
+
+
+console.log(kilometresTravel);
+console.log(ageTraveler);
 
 // Impostazione del calolo 
-let finalPrice = (kilometersTravel * 21) / 100;
+let finalPrice = (kilometresTravel * 21) / 100;
 console.log(finalPrice);
 
 let finalPriceTeen = ''; 
-let finalPriceElderly = '';
+let finalPriceElderly = ''; 
 
 const twentyDiscount = parseInt(20);
 const fortyDiscount = parseInt(40);
-const resultParagraph = document.getElementById(`result`);
+
+const resultParagraph = document.getElementById('result');
 
 
-if (ageTraveler <= 18) {
-  finalPriceTeen = finalPrice - ((twentyDiscount / 100) * finalPrice);
 
-} else if (ageTraveler >= 65) {
-  finalPriceElderly = finalPrice - ((fortyDiscount / 100) * finalPrice);
+if (isNaN(kilometresTravel) || isNaN(ageTraveler)) {
+  alert ('Il carattere inserito non è valido.')
 
 } else {
-  resultParagraph.innerText = `Il prezzo a te dedicato è: ${finalPrice}€.`;
+  if (ageTraveler <= 18) {
+    finalPriceTeen = finalPrice - ((twentyDiscount / 100) * finalPrice);
 
+    resultParagraph.innerHTML = `Hai ricevuto il 20% di sconto! Il prezzo del biglietto non è più <del> ${finalPrice}€ </del>, pagherai soltanto ${finalPriceTeen}€.`;
+
+} else if (ageTraveler >= 65) {
+    finalPriceElderly = finalPrice - ((fortyDiscount / 100) * finalPrice);
+    
+    resultParagraph.innerHTML = `Hai ricevuto il 40% di sconto! Il prezzo del biglietto non è più <del> ${finalPrice}€ </del>, pagherai soltanto ${finalPriceElderly}€.`;
+    
+} else {
+    resultParagraph.innerText = `Il prezzo a te dedicato è:  ${finalPrice}€.`;
+}
 }
